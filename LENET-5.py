fff1f94f90b9b5a76a7d26d087d9ec250f598a22
PranavGrandhi <39693342+PranavGrandhi@users.noreply.github.com>
2018-10-02 14:11:11 +0530

    Add files via upload

---
+++ LENET-5.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+import keras
+
+np.random.seed(2)
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import itertools
+
+from keras.datasets import mnist, cifar10
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape
+from keras.optimizers import SGD, RMSprop
+from keras.regularizers import l2
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D, AveragePooling2D
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, Lambda, Flatten
+from keras.optimizers import Adam ,RMSprop
+from keras.callbacks import EarlyStopping
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.normalization import BatchNormalization
+from keras.utils.np_utils import to_categorical
+from keras.callbacks import ReduceLROnPlateau
+
+
+train = pd.read_csv("fashion-mnist_train.csv")
+x_test = pd.read_csv("fashion-mnist_test.csv")
+
+y_train = train["label"]
+y_test = x_test["label"]
+
+x_train = train.drop(labels = ["label"],axis = 1)
+x_test = x_test.drop(labels = ["label"],axis = 1)
+
+del train
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.values.reshape(-1,28,28,1)
+x_test = x_test.values.reshape(-1,28,28,1)
+
+y_train = to_categorical(y_train, num_classes = 10)
+y_test = to_categorical(y_test, num_classes = 10)
+
+model = Sequential()
+
+model.add(Convolution2D(6, 5, 5, border_mode='valid', input_shape = (28,28,1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Activation("sigmoid"))
+
+model.add(Convolution2D(16, 5, 5, border_mode='valid'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Activation("sigmoid"))
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(120, 1, 1, border_mode='valid'))
+
+model.add(Flatten())
+model.add(Dense(84))
+model.add(Activation("sigmoid"))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+model.summary()
+
+optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+
+epochs = 3
+batch_size = 86
+
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+
+datagen.fit(x_train)
+
+history = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (x_test,y_test),
+                              verbose = 1, steps_per_epoch=x_train.shape[0] // batch_size
+                              , callbacks=[learning_rate_reduction])
---
+++ randomarchi.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+import keras
+
+np.random.seed(2)
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import itertools
+
+from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+
+
+
+train = pd.read_csv("fashion-mnist_train.csv")
+x_test = pd.read_csv("fashion-mnist_test.csv")
+
+y_train = train["label"]
+y_test = x_test["label"]
+
+x_train = train.drop(labels = ["label"],axis = 1)
+x_test = x_test.drop(labels = ["label"],axis = 1)
+
+del train
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.values.reshape(-1,28,28,1)
+x_test = x_test.values.reshape(-1,28,28,1)
+
+y_train = to_categorical(y_train, num_classes = 10)
+y_test = to_categorical(y_test, num_classes = 10)
+
+model = Sequential()
+
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', 
+                 activation ='relu', input_shape = (28,28,1)))
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', 
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same', 
+                 activation ='relu'))
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same', 
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+model.add(Dropout(0.25))
+
+
+model.add(Flatten())
+model.add(Dense(256, activation = "relu"))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation = "softmax"))
+
+model.summary()
+
+optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+
+epochs = 1
+batch_size = 86
+
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+
+datagen.fit(x_train)
+
+history = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (x_test,y_test),
+                              verbose = 1, steps_per_epoch=x_train.shape[0] // batch_size
+                              , callbacks=[learning_rate_reduction])
